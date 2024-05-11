15c7d93b15713ae0abe6fc64d9de21bae7271d70
Ganesh babu <ganeshbabugb2525@gmail.com>
2024-05-12 02:57:47 +0530

    Build Pipeline

--- src/main/java/com/ganesh/test/config/AWSRunner.java
+++ src/main/java/com/ganesh/test/config/AWSRunner.java
@@ -1,6 +1,8 @@
 package com.ganesh.test.config;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,11 +14,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AWSRunner implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(AWSRunner.class);
     private final S3Client s3Client;
 
     @Override
     public void run(String... args) throws Exception {
         List<Bucket> buckets = s3Client.listBuckets().buckets();
-        buckets.forEach(bucket -> System.out.println("bucket = " + bucket.name()));
+        buckets.forEach(bucket -> log.info("bucket = {}", bucket.name()));
     }
 }
