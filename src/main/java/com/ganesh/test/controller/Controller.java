ca616663b184a2e7f9987a25adb508316c287ffe
Ganesh babu <ganeshbabugb2525@gmail.com>
2024-05-11 16:28:13 +0530

    Code Enhancement.

--- src/main/java/com/ganesh/test/controller/Controller.java
+++ src/main/java/com/ganesh/test/controller/Controller.java
@@ -10,8 +10,18 @@ import java.util.Map;
 @RequestMapping("/")
 public class Controller {
 
-    @GetMapping("/hello")
+    @GetMapping()
+    public String home() {
+        return "Hello Ganesh!";
+    }
+
+    @GetMapping("/test")
     public Map<String, String> test() {
-        return Map.of("Testing", "Working Fine!");
+        return Map.of("Testing", "Working Fine Ganesh!");
+    }
+
+    @GetMapping(value = "/test2", params = "v=2")
+    public Map<String, String> test2() {
+        return Map.of("Testing Version 2", "Working Fine Ganesh babu!");
     }
 }
