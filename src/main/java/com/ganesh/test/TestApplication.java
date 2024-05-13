0cec223c9233ff23a374338790633ea6b7d3246e
Ganesh babu <ganeshbabugb2525@gmail.com>
2024-05-13 17:35:43 +0530

    SUMMA TESTING.

--- src/main/java/com/ganesh/test/TestApplication.java
+++ src/main/java/com/ganesh/test/TestApplication.java
@@ -1,16 +1,11 @@
 package com.ganesh.test;
 
-import com.ganesh.test.config.AWSCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AWSCredentials.class)
 public class TestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
-
 }
